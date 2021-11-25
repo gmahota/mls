ec02c7bc18070f1bea2207e262c4712ae6319bfd
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-11-25 21:31:10 +0200

    add patients

--- client/src/services/repository.js
+++ client/src/services/repository.js
@@ -6,10 +6,11 @@ const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 const baseDomain = publicRuntimeConfig.SERVER_URI; // API for products
 export const basePostUrl = publicRuntimeConfig.SERVER_URI; // API for post
 export const baseStoreURL = publicRuntimeConfig.SERVER_URI; // API for vendor(store)
+export const baseAthletesURL = publicRuntimeConfig.ATHLETES_URL; // API for vendor(store)
 
 export const customHeaders = {
   Accept: "application/json",
-  Authorization: `Bearer ${publicRuntimeConfig.Auth}`,
+  Authorization: `Bearer ${publicRuntimeConfig.ATHLETES_API}`,
 };
 
 export const baseUrl = `${baseDomain}`;
